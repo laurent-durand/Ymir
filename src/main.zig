ec8639f95fe855ecf743e7cf38556eb1de62bbdd
laurent-durand <2.97517149e+08+laurent-durand@users.noreply.github.com>
2026-06-28 23:38:26 +0000

    Implement core logic for Ymir

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
-
 pub fn main() !void {
-    std.debug.print("Hello from Ymir!\n", .{});
+    var buffer: [1024]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+    const memory = try allocator.alloc(u8, 100);
+    std.debug.print("--- Ymir: Custom Allocator (Zig) ---\n", .{});
+    std.debug.print("Allocated 100 bytes at {*}\n", .{memory.ptr});
 }
